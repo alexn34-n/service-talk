b90b8485800d4852d5b3e9ae61e6ac836eff1208
alexn <alexn3469@mail.ru>
2023-02-22 18:46:51 +0300

    Init commit

---
+++ src/main/java/dev/alexn/servicetalk/ServiceTalkApplication.java
@@ -0,0 +1,14 @@
+package dev.alexn.servicetalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceTalkApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ServiceTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/alexn/servicetalk/ServiceTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alexn.servicetalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
